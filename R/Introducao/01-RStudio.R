c91b37a40cf26699027f9cdf528432a5c0743e24
Deivid Willyan <deiviid_@hotmail.com>
2018-11-23 09:00:43 -0200

    Create 01-RStudio.R

---
+++ R/Introducao/01-RStudio.R
@@ -0,0 +1,36 @@
+# Explorando o 01-RStudio.R
+
+# Nome dos contibutors
+contributors()
+
+# Licença
+license()
+
+# Informações sobre a sessão
+sessionInfo()
+
+# Imprimir
+print('R - Ciencia de Dados')
+
+# Plot Grafico
+plot(1:30)
+hist(rnorm(10))
+
+# Instalar pacotes
+install.packages('randomForest')
+install.packages('ggplot2')
+
+# Carregar pacote
+library(ggplot2)
+
+# Descarregar pacote
+detach(package:ggplot2)
+
+# Help
+help(mean)
+?mean
+help.search('randomForest')
+help.search('matplot')
+??matplot
+RSiteSearch('matplot')
+example('matplot')
